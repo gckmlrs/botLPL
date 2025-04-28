2abfb0df24981992ba415ca99bb17dc1720dd43f
corentin. <166141364+gckmlrs@users.noreply.github.com>
2025-04-28 07:50:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ async def on_ready():
         print("❌ Salon introuvable.")
 
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_weekly_planning, 'cron', day_of_week='mon', hour=7, minute=0)
+    scheduler.add_job(send_weekly_planning, 'cron', day_of_week='mon', hour=5, minute=52)
     scheduler.start()
     print("🕒 Scheduler activé : Envoi tous les lundis à 7h !")
 
